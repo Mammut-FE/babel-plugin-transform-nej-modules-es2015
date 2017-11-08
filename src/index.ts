526df6655020112517a1898f9be80ad3a54cf5d6
lleohao <lleohao@hotmail.com>
2017-11-08 10:28:21 +0800

    feat(main): Add export default

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import template from 'babel-template';
 
 const buildCommonjs = template(`
     IMPORT_LIST
+    FN_BODY
 `)
 
 export default function ({ types: t }) {
@@ -11,22 +12,33 @@ export default function ({ types: t }) {
                 exit: (path) => {
                     let deps;
                     let alias;
+                    let fnBody: any[];
 
                     path.traverse({
-                        CallExpression: ({ node }) => {
+                        CallExpression: (_path) => {
+                            const { node } = _path;
+
                             if (node.callee.name === 'define') {
                                 deps = node.arguments[0].elements.map(dep => {
                                     return dep.value;
                                 });
 
                                 if (t.isFunctionExpression(node.arguments[1])) {
+                                    fnBody = node.arguments[1].body.body;
+
                                     alias = node.arguments[1].params.map(param => {
                                         return param.name;
                                     });
                                 }
                             }
                         }
-                    })
+                    });
+
+                    let lastFnbody = fnBody[fnBody.length - 1];
+
+                    if (t.isReturnStatement(lastFnbody)) {
+                        fnBody[fnBody.length - 1] = t.exportDefaultDeclaration(lastFnbody.argument);
+                    }
 
                     const IMPORT_LIST = deps.map((dep, i) => {
                         const specifiers = [];
@@ -36,14 +48,15 @@ export default function ({ types: t }) {
                         return t.ImportDeclaration(specifiers, t.StringLiteral(dep))
                     });
 
-                    const { body, directives } = path.node;
+                    const { directives } = path.node;
 
                     path.node.directives = [];
                     path.node.body = [];
 
                     path.pushContainer("body",
                         buildCommonjs({
-                            IMPORT_LIST
+                            IMPORT_LIST,
+                            FN_BODY: fnBody
                         })
                     );
                 }
