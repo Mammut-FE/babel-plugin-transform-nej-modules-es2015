f227b35b95daa4e87c1f2c31f42b82699ccb8395
lleohao <lleohao@hotmail.com>
2017-11-13 14:27:44 +0800

    feat: 增加处理nej注入功能
    处理如下文中的问题:https://github.com/genify/nej/blob/master/doc/DEPENDENCY.md#接口

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { ReturnStatement } from '../../babel-helper-nej-transforms/node_modules/
 
 const buildWrapper = template(`
     IMPORT_LIST
+
+    NEJ_INJECT
+
     FN_BODY
 `);
 
@@ -13,10 +16,11 @@ export default function ({ types: t }) {
             Program: {
                 exit: (path, { opts }) => {
                     const {
-                        custormModule, textModule, nejModule, fnBody: FN_BODY
+                        custormModule, textModule, nejModule, nejInject, fnBody: FN_BODY
                     } = fetchNejDependence(path, opts);
 
                     const IMPORT_LIST = [];
+                    const NEJ_INJECT = [];
 
                     /**
                      * 处理 nej 依赖, 转换成依赖 nejm https://github.com/lleohao/nejm
@@ -30,7 +34,10 @@ export default function ({ types: t }) {
                      * import { klass as _k, util as _u, utilTemplateTpl as _t, utilRegularModule as _m } from "nejm";
                      */
                     if (nejModule.length) {
-                        const specifiers = nejModule.map(({ name, nejmName }) => {
+                        const specifiers = nejModule.map(({ name, nejmName, source }) => {
+                            if (!name) {
+                                throw path.buildCodeFrameError(`${source} 被引入, 但为被使用!`);
+                            }
                             return t.importSpecifier(t.Identifier(name), t.Identifier(nejmName))
                         })
                         IMPORT_LIST.unshift(t.ImportDeclaration(specifiers, t.StringLiteral('nejm')));
@@ -51,6 +58,9 @@ export default function ({ types: t }) {
                      */
                     if (textModule.length) {
                         textModule.forEach(({ source, name }) => {
+                            if (!name) {
+                                throw path.buildCodeFrameError(`${source} 被引入, 但为被使用!`);
+                            }
                             const specifiers = [t.ImportNamespaceSpecifier(t.Identifier(name))];
                             IMPORT_LIST.push(t.ImportDeclaration(specifiers, t.StringLiteral(source)));
                         });
@@ -68,12 +78,30 @@ export default function ({ types: t }) {
                         });
                     }
 
+                    if (nejInject.length) {
+                        if (nejInject.length > 4) {
+                            throw path.buildCodeFrameError(`NEJ 的注入变量超过了4个!`);
+                        }
+                        const injects = [
+                            t.objectExpression([]),
+                            t.objectExpression([]),
+                            t.functionExpression(null, [], t.blockStatement([t.returnStatement((t.booleanLiteral(false)))])),
+                            t.arrayExpression([])
+                        ];
+
+                        nejInject.forEach(name => {
+                            NEJ_INJECT.push(t.variableDeclaration('const', [t.variableDeclarator(t.identifier(name), injects.shift())]));
+                        });
+                    }
+
                     const { directives } = path.node;
 
                     // 处理 return
                     let lastFnbody = FN_BODY[FN_BODY.length - 1];
                     if (t.isReturnStatement(lastFnbody)) {
-                        FN_BODY[FN_BODY.length - 1] = t.exportDefaultDeclaration((lastFnbody as ReturnStatement).argument);
+                        FN_BODY[FN_BODY.length - 1] = t.ExportDefaultDeclaration((lastFnbody as ReturnStatement).argument);
+                    } else if (nejInject.length) { // 没有导出时, 并且通过nej注入了 _p, 则默认抛出 _p
+                        FN_BODY.push(t.ExportDefaultDeclaration(t.Identifier(nejInject[0])));
                     }
 
                     path.node.directives = [];
@@ -82,6 +110,7 @@ export default function ({ types: t }) {
                     path.pushContainer("body",
                         buildWrapper({
                             IMPORT_LIST,
+                            NEJ_INJECT,
                             FN_BODY
                         })
                     );
